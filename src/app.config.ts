fedb35c06db675b301ca0c52a34f3eb7f415a215
zihua <zihua1022@qq.com>
2023-06-06 17:07:04 +0800

    fix：修改微信小程序的主题配置方式

--- src/app.config.ts
+++ src/app.config.ts
@@ -5,11 +5,12 @@ export default defineAppConfig({
   tabBar: routerConfig.tabBarConfig,
   usingComponents: {},
   window: {
-    backgroundTextStyle: "dark",
-    backgroundColor: "#202123",
-    navigationBarBackgroundColor: "#202123",
-    navigationBarTitleText: "WeChat",
-    navigationBarTextStyle: "black",
+    // backgroundTextStyle: "dark",
+    backgroundColor: "#141414",
+    // navigationBarBackgroundColor: "#202123",
+    // navigationBarTitleText: "WeChat",
+    // navigationBarTextStyle: "black",
     navigationStyle: "custom",
   },
+  darkmode: true, // 添加这一行
 });
